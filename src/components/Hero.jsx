4494f5f7cae4559d2d6902d393af007384358a61
Santiago Penino <santiago.penino@gmail.com>
2024-02-26 20:56:49 -0300

    probando responsive hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,38 +24,36 @@ const Hero = ({
         ease: "easeInOut",
       }}
     >
-      <div className={styles.hero_container}>
-        <div className={styles.image_container}>
-          <motion.img
-            src={imageUrl}
-            className={styles.hero_image}
-            initial={{ filter: "blur(10px)" }}
-            animate={{ filter: "blur(0px)" }}
-            exit={{ filter: "blur(25px)" }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-          />
-        </div>
-        <div
-          className={styles.text_container}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+      {/* <div className={styles.hero_container}> */}
+      <div className={styles.image_container}>
+        <motion.img
+          src={imageUrl}
+          className={styles.hero_image}
+          initial={{ filter: "blur(10px)" }}
+          animate={{ filter: "blur(0px)" }}
+          exit={{ filter: "blur(25px)" }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
-        >
-          <h2 className={styles.hero_title}>{title}</h2>
-          <p className={styles.hero_text}>{normalText}</p>
-          <p className={styles.hero_bold}>{boldText}</p>
-          {isDesktop && <button className={styles.cta}>{button}</button>}
-        </div>
-        {!isDesktop && (
-          <div className="button_container">
-            <button className={styles.cta_mobile}>{button}</button>
-            <Markers sections={sections} currentSection={currentSection} />
-          </div>
-        )}
+        />
       </div>
-
-
+      <div
+        className={styles.text_container}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <h2 className={styles.hero_title}>{title}</h2>
+        <p className={styles.hero_text}>{normalText}</p>
+        <p className={styles.hero_bold}>{boldText}</p>
+        {isDesktop && <button className={styles.cta}>{button}</button>}
+      </div>
+      {!isDesktop && (
+        <div className="button_container">
+          <button className={styles.cta_mobile}>{button}</button>
+          <Markers sections={sections} currentSection={currentSection} />
+        </div>
+      )}
+      {/* </div> */}
     </motion.div>
   );
 };
