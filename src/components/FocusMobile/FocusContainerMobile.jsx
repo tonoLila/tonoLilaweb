89a0fea66c09aabf4c2bb71af1b8e3622f8f38c7
Santiago Penino <santiago.penino@gmail.com>
2024-03-02 05:09:52 -0300

    foco mobile

--- src/components/FocusMobile/FocusContainerMobile.jsx
+++ src/components/FocusMobile/FocusContainerMobile.jsx
@@ -1,25 +1,25 @@
 import { motion } from "framer-motion";
 import styles from "@styles/focus.module.css";
-import Focus from "@components/Focus";
-const rectangularSVG = ({ children }) => {
+import FocusMobile from "./FocusMobile";
+const FocusContainerMobile = ({ children }) => {
   return (
     <svg
       className={styles.focus_svg}
       width="100%"
       height=" 100%"
-      viewBox="0 0 1162 624"
+      viewBox="0 0 344 677"
       xmlns="http://www.w3.org/2000/svg"
     >
       <g>
-        <rect x="0" y="0" width="1162" height="624" fill="transparent" />
-        <Focus />
+        <rect x="0" y="0" width="344" height="677" fill="transparent" />
+        <FocusMobile />
       </g>
       <foreignObject x="0" y="0" width="100%" height="100%">
         <motion.div
           initial={{ filter: "blur(10px)", opacity: 0 }}
           animate={{ filter: "blur(0px)", opacity: 1 }}
           transition={{ duration: 1, type: "tween", ease: "easeInOut" }}
-          className={styles.focus_container}
+          className={styles.focus_container_mobile}
         >
           {children}
         </motion.div>
@@ -28,4 +28,4 @@ const rectangularSVG = ({ children }) => {
   );
 };
 
-export default rectangularSVG;
+export default FocusContainerMobile;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Markers from "@components/Markers";
 import styles from "@styles/hero.module.css";
-import RectangularSVG from "./FocusSVG";
+import FocusContainerDesktop from "./FocusDesktop/FocusContainerDesktop";
 
 const Hero = ({
   title,
@@ -26,7 +26,7 @@ const Hero = ({
       }}
     >
       <div className={styles.hero_container}>
-        <RectangularSVG>
+        <FocusContainerDesktop>
           <div className={styles.image_container}>
             <img src={imageUrl} className={styles.hero_image} />
           </div>
@@ -42,7 +42,7 @@ const Hero = ({
             <p className={styles.hero_bold}>{boldText}</p>
             {isDesktop && <button className={styles.cta}>{button}</button>}
           </div>
-        </RectangularSVG>
+        </FocusContainerDesktop>
         {!isDesktop && (
           <div className="button_container">
             <button className={styles.cta_mobile}>{button}</button>
