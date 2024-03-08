c7e3a2a8e456b3f1a60bac1af82a2e8434f32c0e
Santiago Penino <santiago.penino@gmail.com>
2024-03-08 05:13:59 -0300

    galeria

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,5 @@
-import { useRef,useEffect } from "react";
-import { motion, useScroll, useTransform, useVelocity } from "framer-motion";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import styles from "@styles/gallery.module.css";
 import { images } from "@utils";
 
@@ -7,14 +7,16 @@ const Gallery = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
 
-
-  const x = useTransform(scrollYProgress, [0, 1], ["3%", "-30%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["75%", "-30%"]);
 
   return (
     <div>
       <section className={styles.gallery_section} ref={targetRef}>
         <div className={styles.gallery_container}>
-          <motion.div className={styles.image_wrapper} style={{ x , transition: { duration: 0.3 }}}>
+          <motion.div
+            className={styles.image_wrapper}
+            style={{ x, transition: { duration: 0.3 } }}
+          >
             {images.map((image, index) => (
               <img
                 className={styles.image}
