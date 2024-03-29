620ebc23afaf9a2e4ca28048e1dfe959bc39bedc
Santiago Penino <santiago.penino@gmail.com>
2024-03-28 23:03:40 -0300

    galeria scroll

--- src/components/HorizontalCarousel.jsx
+++ src/components/HorizontalCarousel.jsx
@@ -23,7 +23,7 @@ const HorizontalCarousel = () => {
       <div className="horizontal_scroll_wrapper">
         {images.map((image, index) => {
           return (
-            <div key={index} className={`${image.class_name} ${styles.image_wrapper}`}>
+            <div key={index} className={`${image.class_name} image_wrapper`}>
               <a href={`/gallery/${image.img}`} target="_blank">
                 <img src={`/gallery/${image.img}`} alt={`Image ${image.img}`} />
               </a>
