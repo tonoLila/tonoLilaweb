dd972d16f39e66e0c65a78765be8221ad88eb218
Santiago Penino <santiago.penino@gmail.com>
2024-04-12 18:18:26 -0300

    contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "@styles/contact.module.css";
 
 const Contacto = () => {
-  return <div>Contacto</div>;
+  return (
+    <div className={styles.contact_container}>
+      <div className={styles.container}>
+          <h1 className={styles.contact_title}> ¡Hagamos Arte!</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Contacto;
