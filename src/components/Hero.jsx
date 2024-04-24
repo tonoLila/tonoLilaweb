b83eeecc7285e21a7bc7b91a0c7b9c048d09c7b4
Santiago Penino <santiago.penino@gmail.com>
2024-04-24 02:00:44 -0300

    modo mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,10 @@
 import { motion } from "framer-motion";
 import Markers from "@components/Markers";
 import styles from "@styles/hero.module.css";
-import FocusContainerDesktop from "./FocusDesktop/FocusContainerDesktop";
-import FocusContainerMobile from "./FocusMobile/FocusContainerMobile";
+import FocusContainer from "./FocusDesktop/FocusContainerDesktop";
 
 const Hero = ({
   title,
-  boldText,
   normalText,
   imageUrl,
   button,
@@ -14,9 +12,31 @@ const Hero = ({
   sections,
   currentSection,
 }) => {
-  const FocusContainer = isDesktop
-    ? FocusContainerDesktop
-    : FocusContainerMobile;
+  const content = (
+    <>
+      <div className={styles.image_container}>
+        <img src={imageUrl} className={styles.hero_image} />
+      </div>
+      <div
+        className={styles.text_container}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <h2 className={styles.hero_title}>{title}</h2>
+        <p className={styles.hero_text}>{normalText}</p>
+        <motion.button whileTap={{ scale: 0.9 }} className={styles.cta}>
+          {button}
+        </motion.button>
+      </div>
+      {!isDesktop && (
+        <div className="button_container">
+          <Markers sections={sections} currentSection={currentSection} />
+        </div>
+      )}
+    </>
+  );
 
   return (
     <motion.div
@@ -31,30 +51,11 @@ const Hero = ({
       }}
     >
       <div className={styles.hero_container}>
-        <FocusContainer>
-          {isDesktop && (
-            <div className={styles.image_container}>
-              <img src={imageUrl} className={styles.hero_image} />
-            </div>
-          )}
-          <div
-            className={styles.text_container}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-          >
-            <h2 className={styles.hero_title}>{title}</h2>
-            <p className={styles.hero_text}>{normalText}</p>
-            <button className={styles.cta}>{button}</button>
-          </div>
-
-          {!isDesktop && (
-            <div className="button_container">
-              <Markers sections={sections} currentSection={currentSection} />
-            </div>
-          )}
-        </FocusContainer>
+        {isDesktop ? (
+          <FocusContainer>{content}</FocusContainer>
+        ) : (
+          content
+        )}
       </div>
     </motion.div>
   );
