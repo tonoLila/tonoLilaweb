31c9398b361f3bced0002483dd45eabac159be29
Santiago Penino <santiago.penino@gmail.com>
2024-05-10 22:31:39 -0300

    cambios en voces

--- src/pages/Voces.jsx
+++ src/pages/Voces.jsx
@@ -9,26 +9,19 @@ const voces = () => {
     <div className={styles.voces_container}>
       <div className={styles.wrapper}>
         <h1 className={styles.title}>Voces Artisticas</h1>
+        <h2 className={styles.sub_title}>Entrevistas:</h2>
         <section className={styles.cards_container}>
           <div className={styles.first_card}>
-            <Cards imageUrl="1" text="Entrevistas a músicos" link="tonolila" />
+            <Cards imageUrl="1" text="Música" link="tonolila" />
           </div>
           <div className={styles.second_card}>
-            <Cards
-              imageUrl="2"
-              text="Entrevistas a artistas plasticos"
-              link="tonolila"
-            />
+            <Cards imageUrl="2" text="Diseño" link="tonolila" />
           </div>
           <div className={styles.third_card}>
-            <Cards imageUrl="3" text="Otras entrevistas" link="tonolila" />
+            <Cards imageUrl="3" text="Otras" link="tonolila" />
           </div>
           <div className={styles.fourth_card}>
-            <Cards
-              imageUrl="4"
-              text="Entrevistas a escultores"
-              link="tonolila"
-            />
+            <Cards imageUrl="4" text="Artes visuales" link="tonolila" />
           </div>
         </section>
       </div>
